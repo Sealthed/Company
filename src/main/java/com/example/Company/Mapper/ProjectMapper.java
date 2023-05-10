8ddf83ea68e19aa56e2fe1d7baf7a1dbf22c035d
Sealthed <rushbeat301@gmail.com>
2023-05-10 13:50:26 +0700

    Add all mappers

--- src/main/java/com/example/Company/Mapper/ProjectMapper.java
+++ src/main/java/com/example/Company/Mapper/ProjectMapper.java
@@ -1,4 +1,19 @@
 package com.example.Company.Mapper;
 
+import com.example.Company.entity.Project;
+import com.example.Company.serviceDTO.ProjectDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface ProjectMapper {
+
+    ProjectMapper INSTANCE = Mappers.getMapper(ProjectMapper.class);
+
+    @Mapping(source = "projectid", target = "id")
+    @Mapping(source = "projectName", target = "name")
+    @Mapping(source = "area", target = "area")
+    ProjectDTO toDTO(Project project);
+
 }
